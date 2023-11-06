4f58b3f1f6c4e84c92f6e59c23f3451c76b51c3b
DavidsonAdValorem <davidson.rafalimanantsoa@advalorem-solutions.com>
2023-11-06 14:34:05 +0300

    add vue js on project

---
+++ assets/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{name}}</h1>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            name: 'test'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
