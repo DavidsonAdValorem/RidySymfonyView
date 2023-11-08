643c93e37877abd81ef49955347e1cf15b5bc577
DavidsonAdValorem <davidson.rafalimanantsoa@advalorem-solutions.com>
2023-11-08 17:03:22 +0300

    use validator for each property

--- src/Entity/Skills.php
+++ src/Entity/Skills.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\SkillsRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: SkillsRepository::class)]
 class Skills
@@ -15,15 +16,18 @@ class Skills
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
     private ?string $name = null;
 
     #[ORM\Column(type: Types::TEXT)]
     private ?string $description = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
     private ?string $type = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank]
     private ?string $side = null;
 
     #[ORM\Column]
